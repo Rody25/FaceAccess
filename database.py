79edcd0aeb410382bd33c41fe1a2ccc2d8a3cf50
Rody25 <158576329+Rody25@users.noreply.github.com>
2025-05-01 16:06:29 +0200

    Update database.py

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ import sqlite3
 from datetime import datetime
 import os
 
-DB_FILE = "faceaccess.db"
+DB_FILE="faceaccess.db"
 
 def opprett_database():
     with sqlite3.connect(DB_FILE) as conn:
-        c = conn.cursor()
+        c=conn.cursor()
         c.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,11 +17,11 @@ def opprett_database():
         """)
         conn.commit()
 
-def lagre_bruker(navn, bildemappe):
+def lagre_bruker(navn,bildemappe):
     opprett_database()
-    tidspunkt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    tidspunkt=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with sqlite3.connect(DB_FILE) as conn:
-        c = conn.cursor()
+        c=conn.cursor()
         c.execute("INSERT INTO users (navn, bildemappe, opprettet_tid) VALUES (?, ?, ?)",
                   (navn, bildemappe, tidspunkt))
         conn.commit()
