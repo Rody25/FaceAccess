a8b67c07d3722b6809f0147413a293a15484840c
Rody25 <158576329+Rody25@users.noreply.github.com>
2025-05-01 16:19:50 +0200

    Update register_faces.py

--- register_faces.py
+++ register_faces.py
@@ -4,26 +4,26 @@ import cv2
 from picamera2 import Picamera2
 from database import lagre_bruker
 
-# Spør om navn
-name = input("Navn på bruker (ingen mellomrom): ")
-path = f"dataset/{name}"
+#Spør brukeren om navn
+name=input("Navn på bruker (ingen mellomrom): ")
+path=f"dataset/{name}"
 os.makedirs(path, exist_ok=True)
 
-# Start Picamera2
-picam2 = Picamera2()
-picam2.preview_configuration.main.size = (640, 480)
-picam2.preview_configuration.main.format = "RGB888"
+#Her starter Picamera2
+picam2=Picamera2()
+picam2.preview_configuration.main.size=(640, 480)
+picam2.preview_configuration.main.format="RGB888"
 picam2.configure("preview")
 picam2.start()
 time.sleep(2)
 
 print("[INFO] Starter bildeopptak. Trykk CTRL+C for å avbryte.")
 
-count = 0
+count=0
 try:
-    while count < 20:
-        frame = picam2.capture_array()
-        img_name = f"{path}/{count}.jpg"
+    while count<20:
+        frame=picam2.capture_array()
+        img_name=f"{path}/{count}.jpg"
         cv2.imwrite(img_name, frame)
         count += 1
         print(f"[INFO] Lagret {img_name}")
